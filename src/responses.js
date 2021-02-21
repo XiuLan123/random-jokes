99ac0a847bdb3573c28e9c38dd1539ea05326c7a
John Zhang <1724118498@qq.com>
2021-02-21 16:46:02 +0800

    push

--- src/responses.js
+++ src/responses.js
@@ -1,45 +1,46 @@
+const qList = [];
+qList[0] = {
+  q: 'What do you call a very small valentine?',
+  a: 'A valen-tiny!!!',
+};
+qList[1] = {
+  q: 'What did the dog say when he rubbed his tail on the sandpaper?',
+  a: 'Ruff, Ruff!!!',
+};
+qList[2] = {
+  q: "Why don't sharks like to eat clowns?",
+  a: 'Because they taste funny!!!',
+};
+qList[3] = {
+  q: 'What did the boy cat say to the girl cat?',
+  a: "You're Purr-fect!!!",
+};
+qList[4] = {
+  q: "What is a frog's favorite outdoor sport?",
+  a: 'Fly Fishing!!!',
+};
+qList[5] = {
+  q: 'I hate jokes about German sausages.',
+  a: 'Theyre the wurst!!!',
+};
+qList[6] = {
+  q: 'Did you hear about the cheese factory that exploded in France?',
+  a: 'There was nothing left but de Brie!!!',
+};
+qList[7] = {
+  q: 'Our wedding was so beautiful',
+  a: 'Even the cake was in tiers!!!',
+};
+qList[8] = {
+  q: 'Is this pool safe for diving?',
+  a: 'It deep ends!!!',
+};
+qList[9] = {
+  q: 'Dad, can you put my shoes on?',
+  a: 'I dont think theyll fit me!!!',
+};
+
 const getRandomJokeJSON = (limit = 1) => {
-  const qList = [];
-  qList[0] = {
-    q: 'What do you call a very small valentine?',
-    a: 'A valen-tiny!!!',
-  };
-  qList[1] = {
-    q: 'What did the dog say when he rubbed his tail on the sandpaper?',
-    a: 'Ruff, Ruff!!!',
-  };
-  qList[2] = {
-    q: "Why don't sharks like to eat clowns?",
-    a: 'Because they taste funny!!!',
-  };
-  qList[3] = {
-    q: 'What did the boy cat say to the girl cat?',
-    a: "You're Purr-fect!!!",
-  };
-  qList[4] = {
-    q: "What is a frog's favorite outdoor sport?",
-    a: 'Fly Fishing!!!',
-  };
-  qList[5] = {
-    q: 'I hate jokes about German sausages.',
-    a: 'Theyre the wurst!!!',
-  };
-  qList[6] = {
-    q: 'Did you hear about the cheese factory that exploded in France?',
-    a: 'There was nothing left but de Brie!!!',
-  };
-  qList[7] = {
-    q: 'Our wedding was so beautiful',
-    a: 'Even the cake was in tiers!!!',
-  };
-  qList[8] = {
-    q: 'Is this pool safe for diving?',
-    a: 'It deep ends!!!',
-  };
-  qList[9] = {
-    q: 'Dad, can you put my shoes on?',
-    a: 'I dont think theyll fit me!!!',
-  };
   const limit2 = Number(limit);
   if (limit2 === 1) {
     const responseObj = qList[Math.floor(Math.random() * 10)];
@@ -54,48 +55,6 @@ const getRandomJokeJSON = (limit = 1) => {
 };
 
 const getRandomJokeXML = (limit = 1) => {
-  const qList = [];
-  qList[0] = {
-    q: 'What do you call a very small valentine?',
-    a: 'A valen-tiny!!!',
-  };
-  qList[1] = {
-    q: 'What did the dog say when he rubbed his tail on the sandpaper?',
-    a: 'Ruff, Ruff!!!',
-  };
-  qList[2] = {
-    q: "Why don't sharks like to eat clowns?",
-    a: 'Because they taste funny!!!',
-  };
-  qList[3] = {
-    q: 'What did the boy cat say to the girl cat?',
-    a: "You're Purr-fect!!!",
-  };
-  qList[4] = {
-    q: "What is a frog's favorite outdoor sport?",
-    a: 'Fly Fishing!!!',
-  };
-  qList[5] = {
-    q: 'I hate jokes about German sausages.',
-    a: 'Theyre the wurst!!!',
-  };
-  qList[6] = {
-    q: 'Did you hear about the cheese factory that exploded in France?',
-    a: 'There was nothing left but de Brie!!!',
-  };
-  qList[7] = {
-    q: 'Our wedding was so beautiful',
-    a: 'Even the cake was in tiers!!!',
-  };
-  qList[8] = {
-    q: 'Is this pool safe for diving?',
-    a: 'It deep ends!!!',
-  };
-  qList[9] = {
-    q: 'Dad, can you put my shoes on?',
-    a: 'I dont think theyll fit me!!!',
-  };
-
   const limit2 = Number(limit);
   if (limit2 === 1) {
     const responseObj = qList[Math.floor(Math.random() * 10)];
